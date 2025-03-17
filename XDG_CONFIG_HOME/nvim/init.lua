c53a55fb9e6212c88fabdadc91d914de9ce9241d
lydd <theo.keanu@gmail.com>
2025-03-17 18:53:36 +0000

    init.lua: first commit

---
+++ XDG_CONFIG_HOME/nvim/init.lua
@@ -0,0 +1,77 @@
+vim.g.have_nerd_font = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.ts = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+
+vim.opt.mouse = 'a'
+
+-- vim.opt.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+-- vim.g.mapleader = " "
+-- vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    {
+      'ellisonleao/gruvbox.nvim',
+      priority = 1000, -- Make sure to load this before all the other start plugins.
+      config = function()
+        require('gruvbox').setup { }
+    vim.o.background = "dark"
+        vim.cmd.colorscheme 'gruvbox'
+      end,
+    },
+    {
+      'neovim/nvim-lspconfig',
+      config = function()
+    local capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
+    require('lspconfig').pyright.setup { capabilities = capabilities }
+      end
+    },
+    require 'plugins.completion',
+  },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
