273c8c8d45a812f6f0c74b9773496198caa92026
lydd <theo.keanu@gmail.com>
2025-03-18 14:13:07 +0000

    completion.lua: Tab/S-Tab for selection and preselect=false

--- XDG_CONFIG_HOME/nvim/lua/plugins/completion.lua
+++ XDG_CONFIG_HOME/nvim/lua/plugins/completion.lua
@@ -5,7 +5,13 @@ return {
 
   version = '*',
   opts = {
-    keymap = { preset = 'default' },
+    completion = { list = { selection = { preselect = false, auto_insert = true }, }, },
+
+    keymap = {
+      preset = 'default',
+      ['<Tab>'] = { 'select_next', 'fallback' },
+      ['<S-Tab>'] = { 'select_prev', 'fallback' },
+    },
 
     appearance = {
       use_nvim_cmp_as_default = true,
