bbde405e99c596ccda96972490ffbabe69fb82ac
lydd <theo.keanu@gmail.com>
2025-03-29 23:31:44 +0000

    init.lua: show relative path

--- XDG_CONFIG_HOME/nvim/init.lua
+++ XDG_CONFIG_HOME/nvim/init.lua
@@ -135,6 +135,9 @@ require("lazy").setup({
       config = function()
         require('lualine').setup {
           options = { theme = 'gruvbox' },
+          sections = {
+            lualine_c = { { 'filename', path = 1, }, },
+          },
         }
       end,
     },
